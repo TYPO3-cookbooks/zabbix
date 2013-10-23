cbca2595a85c738b85437a2f789b5a83bb6ae571
Peter Niederlag <netservice@niekom.de>
2013-10-23 10:25:57 +0200

    set zabbix agent version to 2.0.9

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -1,7 +1,7 @@
 default['zabbix']['agent']['install']           = true
 
 default['zabbix']['agent']['branch']            = "ZABBIX%20Latest%20Stable"
-default['zabbix']['agent']['version']           = "2.0.3"
+default['zabbix']['agent']['version']           = "2.0.9"
 default['zabbix']['agent']['servers']           = []
 default['zabbix']['agent']['servers_active']    = []
 default['zabbix']['agent']['hostname']          = node['fqdn']
